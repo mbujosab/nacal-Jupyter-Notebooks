5efdc313ba8a3224b383c5599791e86bd787da27
Marcos Bujosa <53706089+mbujosab@users.noreply.github.com>
2021-07-30 12:03:02 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
-
-from IPython.display import YouTubeVideo
